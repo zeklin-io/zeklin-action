ff089054d35db4aa93bd342a8034e58cb932ff10
jules Ivanic <jules.ivanic@gmail.com>
2023-08-30 16:34:41 +0400

    Parse inputs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,54 @@
-import * as core from "@actions/core"
-import * as Effect from "effect/Effect"
+import * as core from "@actions/core";
+import { Data, Either, Option, pipe } from "effect";
+import * as Effect from "effect/Effect";
+
+class Inputs extends Data.TaggedClass("Inputs")<{
+  apikey: string
+  cmd: string
+  workdir: Option.Option<string>
+}> {
+}
+
+const inputs: () => Either.Either<Error, Inputs> = () => {
+  const unsafeRequiredInput = (name: string) => {
+    const v = core.getInput(name, { required: true, trimWhitespace: true });
+
+    if (v.length === 0) throw new Error(`Input ${name} is required`);
+    else return v;
+  };
+
+  const optionalInput = (name: string) => {
+    const v = core.getInput(name, { required: false, trimWhitespace: true });
+
+    if (v.length === 0) return Option.none();
+    else return Option.some(v);
+  };
+
+  try {
+    return Either.right(
+      new Inputs({
+        apikey: unsafeRequiredInput("apikey"),
+        cmd: unsafeRequiredInput("cmd"),
+        workdir: optionalInput("workdir")
+      })
+    );
+  } catch (error) {
+    return Either.left(error as Error);
+  }
+};
+
+const debug =  (message: string) => Effect.sync(() => core.debug(message));
+const setFailed = (message: string) => Effect.sync(() => core.setFailed(message)); 
 
 /**
  * The main function for the action.
  */
-export const main: Effect.Effect<never, Error, void> =
-  Effect.logInfo("Hello, world!")
+export const main: Effect.Effect<never, Error, void> = pipe(
+  Effect.flatten(Effect.sync(inputs)),
+  Effect.tapError((error) => setFailed(`Failed to parse inputs: ${error}`)),
+  Effect.tap((inputs) => debug(`Inputs: ${inputs}`)),
+);
 
 Effect.runPromise(main).catch((error) => {
-  if (error instanceof Error) core.setFailed(error.message)
-})
+  if (error instanceof Error) core.setFailed(error.message);
+});
